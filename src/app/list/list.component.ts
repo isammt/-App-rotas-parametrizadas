196586bbf1d6e0e7b9c550af49fe524edd00743d
isammt <bella.trigo14@gmail.com>
2022-03-11 22:23:33 -0300

    1 file modified

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -4,14 +4,11 @@ import { listData } from '../list';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
-OnInit {
   list = listData;
   constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
